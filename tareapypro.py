45b15fe3d9eebfa0104222daf51abd5d93a2f94d
Freeza <franco.bassino.g@gmail.com>
2024-10-02 18:22:47 -0300

    Update tareapypro.py

--- tareapypro.py
+++ tareapypro.py
@@ -1,5 +1,12 @@
-memes = {"CRINGE": "algo excepcionalmente raro o que causa verguenza",
-         "LOL": "respuesta comun a algo gracioso (laugh out loud, lots of laughter, league of le-)"
+memes = {"CRINGE": "algo excepcionalmente raro",
+         "LOL": "respuesta comun a algo gracioso",
+         "GG": "good game (bien jugado)",
+         "EZ": "easy (facil)",
+         "BRO": "brother/hermano (para referirse a alguien)",
+         "BTW": "by the way (por cierto)",
+         "TBH": "to be honest (para ser honesto)",
+         "IMO": "in my opinion (en mi opinion)",
+         "ASAP": "as soon as possible (tan pronto posible)"
         }
 
 word = input("Escribe una palabra que no sepas en mayusculas\n(por favor escribe cringe o lol te lo ruego)")
