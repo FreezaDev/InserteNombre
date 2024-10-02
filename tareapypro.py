79d21ded954d58f5c0716392caaaea20b0687585
Freeza <franco.bassino.g@gmail.com>
2024-10-02 18:13:27 -0300

    Create tareapypro.py

---
+++ tareapypro.py
@@ -0,0 +1,10 @@
+memes = {"CRINGE": "algo excepcionalmente raro o que causa verguenza",
+         "LOL": "respuesta comun a algo gracioso (laugh out loud, lots of laughter, league of le-)"
+        }
+
+word = input("Escribe una palabra que no sepas en mayusculas\n(por favor escribe cringe o lol te lo ruego)")
+
+if word in memes.keys():
+    print("GRAX!!!!!!!!!!! AHORA PUUEDO MANTENER A MI FAMILIA\n", memes[word])
+else:
+    print("TE VOY A ENCONTRAR RAHHHHH")
